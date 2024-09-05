142bddaa64f630f60b700361974493cb6afefc1f
AntoshAlgo2Bots <antosh.pandey@algo2botsinfotech.com>
2024-09-05 20:15:43 +0530

    hi

---
+++ Item Master/script.js
@@ -0,0 +1,228 @@
+function holder() {
+    var x = document.getElementById("holder");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    var y = document.getElementById("wire");
+    y.style.display = "none"
+
+
+    var z = document.getElementById("bulb");
+    z.style.display = "none"
+
+    console.log("asdf")
+}
+
+function wire() {
+    var x = document.getElementById("wire");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    var y = document.getElementById("holder");
+    y.style.display = "none"
+
+
+    var z = document.getElementById("bulb");
+    z.style.display = "none"
+
+    console.log("asdf")
+}
+
+function bulb() {
+    var x = document.getElementById("bulb");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    var y = document.getElementById("wire");
+     y.style.display = "none"
+
+    var z = document.getElementById("holder");
+     z.style.display = "none"
+
+    console.log("asdf")
+}
+
+
+function urn() {
+    var x = document.getElementById("urn");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    
+    var y = document.getElementById("pipe");
+    y.style.display = "none";
+
+    var z = document.getElementById("arm");
+    z.style.display = "none";
+
+    var w = document.getElementById("arm");
+    w.style.display = "none";
+}
+
+function pipe() {
+    var y = document.getElementById("pipe");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+    
+    var x = document.getElementById("urn");
+    x.style.display = "none";
+    
+    var z = document.getElementById("arm");
+    z.style.display = "none";
+
+    var w = document.getElementById("arm");
+    w.style.display = "none";
+}
+
+function arm() {
+    var y = document.getElementById("arm");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+    var x = document.getElementById("urn");
+    x.style.display = "none";
+
+    var z = document.getElementById("pipe");
+    z.style.display = "none";
+
+    var w = document.getElementById("shade");
+    w.style.display = "none";
+    
+}
+
+function shade() {
+    var y = document.getElementById("shade");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+    var x = document.getElementById("urn");
+    x.style.display = "none";
+
+    var z = document.getElementById("pipe");
+    z.style.display = "none";
+
+    var w = document.getElementById("arm");
+    w.style.display = "none";
+}
+
+function plate() {
+    var y = document.getElementById("plate");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+    
+    var z = document.getElementById("centre_pipe");
+    z.style.display = "none"
+}
+
+function centre_pipe() {
+    var y = document.getElementById("centre_pipe");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+
+    var z = document.getElementById("plate");
+    z.style.display = "none"
+}
+
+function paper() {
+    var y = document.getElementById("paper");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+    
+    var z = document.getElementById("ball_pen");
+    z.style.display = "none"
+}
+
+function ball_pen() {
+    var y = document.getElementById("ball_pen");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+
+    var z = document.getElementById("paper");
+    z.style.display = "none"
+}
+
+
+function wall_light() {
+    var y = document.getElementById("wall_light");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+
+    var z = document.getElementById("chandelier");
+    z.style.display = "none"
+
+    var w = document.getElementById("table_lamps");
+    w.style.display = "none"
+}
+
+function chandelier() {
+    var y = document.getElementById("chandelier");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+
+    var z = document.getElementById("table_lamps");
+    z.style.display = "none"
+
+    var x = document.getElementById("wall_light");
+    x.style.display = "none"
+}
+
+
+function table_lamps() {
+    var y = document.getElementById("table_lamps");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+
+
+    var z = document.getElementById("wall_light");
+    z.style.display = "none"
+
+    var x = document.getElementById("chandelier");
+    x.style.display = "none"
+
+}
